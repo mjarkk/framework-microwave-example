5b15ebf79090ce098eaddec764d4efbb7e7a22a0
mjarkk <mkopenga@gmail.com>
2018-10-03 14:13:26 +0200

    Updated progress to github

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	microw "github.com/mjarkk/framework-microwave"
+	T "github.com/mjarkk/framework-microwave/pkg/types"
 )
 
 func main() {
-	microw.Init()
+	microw.Init(&T.Init{
+		MigrationPath: "./database/migration/",
+	})
 }
